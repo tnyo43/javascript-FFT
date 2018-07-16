7d1566af60bc2475243e97ae870a122edde1fbd3
tyno43 <skriulle5@gmail.com>
2018-07-17 03:54:01 +0900

    implement FFT

--- src/fft.js
+++ src/fft.js
@@ -6,27 +6,81 @@ let EPS = Math.pow(10, -8);
 class FFT {
   constructor(N) {
     this.N = N;
-    this.init();
   }
 
   init() {
-    this.valid = this.N >= 2 && this.N <= 32
+    let N = this.N;
+    this.valid = N >= 2 && N <= 32
     if (this.valid) {
       this.points = [];
       this.basis = [];
-      for (var i = 0; i < this.N; i++) {
+
+      // FFT : f = matrixinv . x
+      // IFFT: x = matrix . f
+      this.matrix = [];
+      this.matrix_inv = [];
+
+      for (var i = 0; i < N; i++) {
         let base = [];
-        let angle = 2.0*PI/this.N*i;
+        let angle = 2.0*PI/N*i;
         base.push(Math.cos(angle));
         base.push(Math.sin(angle));
         this.basis.push(base);
-        this.points.push(0);
+        this.points.push(null);
+      }
+
+      for (var j = 0; j < N; j++) {
+        let l1 = [];
+        let l2 = [];
+        for (var i = 0; i < N; i++) {
+          let pair = this.basis[(i*j)%N].concat();
+          l1.push(pair);
+          pair = this.basis[(N*N-i*j)%N].concat();
+          l2.push(pair);
+
+        }
+        this.matrix.push(l1);
+        this.matrix_inv.push(l2);
       }
     }
+    this.spectrum = null;
   }
 
   is_valid() {
     return this.valid;
+  }
+
+  put(index, x) {
+    this.points[index] = x;
+  }
+
+  is_ready() {
+    for (var i = 0; i < this.N; i++)
+      if (this.points[i] == null) return false;
+    return true;
+  }
+
+  fft() {
+    if (!this.is_ready()) return false;
+    let N = this.N;
+    this.spectrum = []
+    for (var i = 0; i < N; i++) {
+      let x = [0.0, 0.0];
+      for (var j = 0; j < N; j++) {
+        for (var k = 0; k < 2; k++) {
+          x[k] += this.matrix_inv[i][j][k]*this.points[j]/N;
+        }
+      }
+      this.spectrum.push(x);
+    }
+    return true;
+  }
+
+  ifft() {
+    if (this.spectrum == null) return false;
+    // ifft
+
+    return true;
   }
 
   set N (n) {
--- test/fft.js
+++ test/fft.js
@@ -93,3 +93,71 @@ describe("初期化", function() {
   });
 });
 
+describe("サンプリング", function() {
+  describe("基底の数とサンプルの数は同じ", function() {
+    it("基底の数とサンプル数が同じなら変換可", function() {
+      let n = 32;
+      let fft = new FFT(n);
+      for (var i = 0; i < n; i++) {
+        fft.put(i, 1);
+      }
+      assert.ok(fft.is_ready());
+    });
+    it("基底の数とサンプル数が異なるなら変換不可", function() {
+      let n = 32
+      let fft = new FFT(n);
+      for (var i = 0; i < n-1; i++) {
+        fft.put(i, 1);
+      }
+      assert.ok(!fft.is_ready());
+    });
+  }); 
+  describe("サンプル点揃ったらFFTできる", function() {
+    it("揃ってないならfalse", function() {
+      let n = 32;
+      let fft = new FFT(n);
+      for (var i = 0; i < n-1; i++) {
+        fft.put(i, 1);
+      }
+      assert.ok(!fft.fft());
+    });
+    describe("揃ったらtrue", function() {
+      it("サンプル点が定数Cならスペクトルムの0番目が[C, 0]、それ以外が0", function() {
+        let n = 32;
+        let c = 10;
+        let fft = new FFT(n);
+        for (var i = 0; i < n; i++) {
+          fft.put(i, c);
+        }
+        assert.ok(fft.fft());
+        assert.ok(almost(fft.spectrum[0][0], c));
+        assert.ok(almost_zero(fft.spectrum[0][1]));
+        for (var i = 1; i < n; i++) {
+          for (var k = 0; k < 2; k++) assert.ok(almost_zero(fft.spectrum[i][k]));
+        }
+      });
+      it("サンプリングがsin関数なら虚部だけ", function() {
+        let n = 32;
+        let fft = new FFT(32);
+        for (var i = 0; i < n; i++) {
+          fft.put(i, Math.sin(2*PI*i/n));
+        }
+        assert.ok(fft.fft());
+        for (var i = 0; i < n; i++) {
+          assert.ok(almost_zero(fft.spectrum[i][0]));
+        }
+      });
+      it("サンプリングがcos関数なら実部だけ", function() {
+        let n = 32;
+        let fft = new FFT(32);
+        for (var i = 0; i < n; i++) {
+          fft.put(i, Math.cos(2*PI*i/n));
+        }
+        assert.ok(fft.fft());
+        for (var i = 0; i < n; i++) {
+          assert.ok(almost_zero(fft.spectrum[i][1]));
+        }
+      });
+    });
+  });
+});
